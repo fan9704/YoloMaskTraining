c89882202733417e41c732a4e6b87bb59b600c9e
fan9704 <cxz123499@gmail.com>
2022-05-09 18:45:26 +0800

    Co-authored-by: Aven910126 <Aven910126@users.noreply.github.com>

--- src/app.py
+++ src/app.py
@@ -1,6 +1,8 @@
 # coding=utf-8
 from __future__ import division, print_function
+# from crypt import methods
 import os
+import re
 import numpy as np
 
 from flask import Flask, redirect, url_for, request, render_template
@@ -18,7 +20,7 @@ classesM = ['N','V','L','R','Paced','A','F']#,'f','j','E','a','J','Q','e','S']
 print('Check http://127.0.0.1:5000/')
 
 config = get_config()
-def model_predict(img_path):
+def model_predict(img_path):#For CSV
     data = uploadedData(img_path, csvbool = True)
     sr = data[0]
 
@@ -31,6 +33,18 @@ def model_predict(img_path):
     data, peaks = preprocess(data, config)
     return predictByPart(data, peaks)
 
+def model_predict2(data):#For JSON
+    #data = uploadedData(img_path, csvbool = False)
+    sr = data[0]
+
+    data = data[1:]
+    size = len(data)
+    if size > 9001:
+        size = 9001
+        data = data[:size]
+    div = size // 1000
+    data, peaks = preprocess(data, config)
+    return predictByPart(data, peaks)
 @app.route('/', methods=['GET'])
 def index():
     # Main page
@@ -61,6 +75,19 @@ def upload():
         
         return result
     return None
+import json
+from flask import jsonify
+@app.route("/api/predict",methods=["POST"])
+def receive():
+    data = json.loads(request.get_data())
+    # request_data=request.get_json()
+    print(data)
+    if request.method=="POST":
+        predicted, result = model_predict2(data["data"])
+        length = len(predicted)
+
+        result = str(length) +" parts of the divided data were estimated as the followings with paired probabilities. \n"+result
+        return jsonify({"status":True})
 
 
 if __name__ == '__main__':
